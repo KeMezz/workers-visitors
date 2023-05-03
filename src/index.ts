13feeb077a1603efa950e508d4396cdcd8cea48c
정형진 <88423849+KeMezz@users.noreply.github.com>
2023-05-04 00:02:33 +0900

    count visits

--- src/index.ts
+++ src/index.ts
@@ -25,22 +25,57 @@ export interface Env {
 // @ts-ignore
 import home from "./home.html";
 
+function handleHome() {
+  return new Response(home, {
+    headers: {
+      "Content-Type": "text/html;chartset=utf-8",
+    },
+  });
+}
+
+async function handleVisit(searchParams: URLSearchParams, env: Env) {
+  const page = searchParams.get("page");
+  if (!page) {
+    return handleBadRequest();
+  }
+
+  const kvPage = await env.view_counter.get(page);
+  let value = 1;
+  if (!kvPage) {
+    await env.view_counter.put(page, value + "");
+    // return new Response(value+"", { status: 201 });
+  } else {
+    value = parseInt(kvPage) + 1;
+    await env.view_counter.put(page, value + "");
+  }
+  return new Response(JSON.stringify({ visits: value }), {
+    headers: { "Content-Type": "application/json" },
+  });
+}
+
+function handleBadRequest() {
+  return new Response(null, { status: 400 });
+}
+
+function handleNotFound() {
+  return new Response(null, { status: 404 });
+}
+
 export default {
   async fetch(
     request: Request,
     env: Env,
     ctx: ExecutionContext
   ): Promise<Response> {
-    const url = new URL(request.url);
-    if (url.pathname === "/") {
-      console.log(env.view_counter);
-      await env.view_counter.put("hello", "how are you?");
-      return new Response(home, {
-        headers: {
-          "Content-Type": "text/html;chartset=utf-8",
-        },
-      });
+    const { pathname, searchParams } = new URL(request.url);
+    console.log(searchParams);
+    switch (pathname) {
+      case "/":
+        return handleHome();
+      case "/visit":
+        return handleVisit(searchParams, env);
+      default:
+        return handleNotFound();
     }
-    return new Response(null, { status: 404 });
   },
 };
